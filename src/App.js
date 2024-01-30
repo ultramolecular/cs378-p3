d0f9ff22da45dc7b30c76d5845863dd16f68b48e
Karim Benharrak <karim.benharrak@sjsu.edu>
2024-01-29 19:04:29 -0600

    commented bootstrap import

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 import MenuItem from './components/MenuItem';
 
-import 'bootstrap/dist/css/bootstrap.min.css'; // This import bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
+// import 'bootstrap/dist/css/bootstrap.min.css'; // This imports bootstrap css styles. You can use bootstrap or your own classes by using the className attribute in your elements.
 
 // Menu data. An array of objects where each object represents a menu item. Each menu item has an id, title, description, image name, and price.
 // You can use the image name to get the image from the images folder.
